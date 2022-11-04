ed399f078aa8ca35665b4cd9ed1e4bf0c02a7a02
Alm4my <almamy@tuta.io>
2022-11-04 05:04:09 +0000

    feat(compte): ajoute des stubs pour reinitialiser le mot de passe
    Ces fonctionalites seront implementes plus tard.

--- comptes/urls.py
+++ comptes/urls.py
@@ -21,6 +21,48 @@ urlpatterns = [
         'logout/',
         auth_views.LogoutView.as_view(template_name='comptes/logout.html'),
         name='logout'
-    )
+    ),
 
+    path(
+        'changer-mdp/',
+        auth_views.PasswordChangeView.as_view(
+            template_name='comptes/changer-mdp.html'
+        ),
+    ),
+
+    path(
+        'changer-mdp-termine/',
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name='home.html'
+        ),
+        name='password_change_done'
+    ),
+
+    path(
+        'reinit-mdp/',
+        auth_views.PasswordResetView.as_view(
+            template_name='comptes/reinit-pw.html'
+        ), name='password_reset'
+    ),
+
+    path(
+        'reinit-mdp-termine/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='comptes/message-reinit-envoye.html'
+        ), name='password_reset_done'
+    ),
+
+    path(
+        'reinit-mdp-confirm/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='comptes/mdp-confirm.html'
+        ), name='password_reset_confirm'
+    ),
+
+    path(
+        'reinit-mdp-confirm-complete/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='comptes/mdp-confirm-complete.html'),
+        name='password_reset_confirm'
+    ),
 ]
