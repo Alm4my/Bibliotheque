c336d8533ab63fa34fd92320bdface0ee826280e
Alm4my <almamy@tuta.io>
2022-11-04 00:03:15 +0000

    refactor(livre): renomme l'attribut nombre de livre en nombre_de_copies

--- livres/models.py
+++ livres/models.py
@@ -8,11 +8,17 @@ class Auteur(models.Model):
     nom = models.CharField(max_length=255)
     prenoms = models.CharField(max_length=255)
 
+    class Meta:
+        unique_together = ('nom', 'prenoms')
+
+    def __str__(self):
+        return f"{self.nom} {self.prenoms}"
+
 
 class Livre(models.Model):
     isbn = models.CharField(max_length=13, primary_key=True)
     titre = models.CharField(max_length=255)
-    nombre = models.IntegerField()
+    nombre_de_copies = models.IntegerField(default=1)
     description = models.TextField(null=True, blank=True)
     auteurs = models.ManyToManyField(Auteur, "livres")
 
